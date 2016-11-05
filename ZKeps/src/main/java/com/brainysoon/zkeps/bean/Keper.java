7ffddea6097a058699c97b42fb6cf147f15b12ed
brainysoon <brainysoon@gmail.com>
2016-11-05 17:24:02 +0800

    modefied-bean-user

--- ZKeps/src/main/java/com/brainysoon/zkeps/bean/Keper.java
+++ ZKeps/src/main/java/com/brainysoon/zkeps/bean/Keper.java
@@ -6,29 +6,77 @@ package com.brainysoon.zkeps.bean;
 public class Keper {
 
     private String userName;
-    private String userPassword;
+    private String password;
+    private String email;
+    private String nickName;
+    private String motto;
+    private String avator;
+    private int stars;
+    private int slead;
 
-    public Keper(String userName, String userPassword) {
-
-        this.userName = userName;
-        this.userPassword = userPassword;
-    }
-
-    public Keper() {
-        super();
-    }
-
-    //get
-    public String getUserName() {
-        return this.userName;
-    }
-
-    //set
+    //setter
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public void setMotto(String motto) {
+        this.motto = motto;
+    }
+
+    public void setAvator(String avator) {
+        this.avator = avator;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+    public void setSlead(int slead) {
+        this.slead = slead;
+    }
+
+    //getter
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getMotto() {
+        return motto;
+    }
+
+    public String getAvator() {
+        return avator;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public int getSlead() {
+        return slead;
     }
 }
